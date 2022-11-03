8b40784ad05cdf76ef2e8d84e8fed11eb74514de
HaneenBhery <habehairi22@twq.idserve.net>
2022-11-03 10:35:53 +0300

    n

--- TEST/SIGN IN.swift
+++ TEST/SIGN IN.swift
@@ -15,7 +15,7 @@ struct SIGNIN: View {
     
     //-----
     
-   // @State var OrsignUp = false
+    // @State var OrsignUp = false
     var body: some View {
         NavigationView{
             ZStack {
@@ -59,59 +59,59 @@ struct SIGNIN: View {
                     
                     VStack {
                         
-        NavigationLink(
-            destination: SIGNUPG().navigationBarHidden(true),
-            label: {
-                
-                Text("Or sign Up")
-                    .frame(width: 150.0, height: 37.0)
-                    .background(Color(red: 0.586, green: 0.75, blue: 0.678))
-                    .foregroundColor(.white)
-                    .cornerRadius(60)
-                    .font(.system(size: 23))
-//                            Button ("Or sign Up"){
-//
-//
-//                            }
-//                            .frame(width: 150.0, height: 37.0)
-//                            .background(Color(red: 0.586, green: 0.75, blue: 0.678))
-//                            .foregroundColor(.white)
-//                            .cornerRadius(60)
-//                            .font(.system(size: 23))
-//                            //.frame(width: 0, height: 250)
-//
-//
-                            
-                            
-                        })
-        .navigationBarHidden(true)
+                        NavigationLink(
+                            destination: SIGNUPG().navigationBarHidden(true),
+                            label: {
+                                
+                                Text("Or sign Up")
+                                    .frame(width: 150.0, height: 37.0)
+                                    .background(Color(red: 0.586, green: 0.75, blue: 0.678))
+                                    .foregroundColor(.white)
+                                    .cornerRadius(60)
+                                    .font(.system(size: 23))
+                                //                            Button ("Or sign Up"){
+                                //
+                                //
+                                //                            }
+                                //                            .frame(width: 150.0, height: 37.0)
+                                //                            .background(Color(red: 0.586, green: 0.75, blue: 0.678))
+                                //                            .foregroundColor(.white)
+                                //                            .cornerRadius(60)
+                                //                            .font(.system(size: 23))
+                                //                            //.frame(width: 0, height: 250)
+                                //
+                                //
+                                
+                                
+                            })
+                        .navigationBarHidden(true)
                         
-                        Button ("sing in"){
-                            
-                            
-                            
-                        }
-                        .frame(width: 300.0, height: 40.0)
+                        NavigationLink(
+                            destination: ContentView().navigationBarHidden(true),
+                            label: {
+                                Text ("sing in")
+                                
+                                    .frame(width: 300.0, height: 40.0)
+                                
+                                    .background(Color(red: 0.586, green: 0.75, blue: 0.678))
+                                    .foregroundColor(.white)
+                                    .cornerRadius(60)
+                                    .font(.largeTitle)
+                                    .padding(.top, 99.0)
+                                
+                            })
                         
-                        .background(Color(red: 0.586, green: 0.75, blue: 0.678))
-                        .foregroundColor(.white)
-                        .cornerRadius(60)
-                        .font(.largeTitle)
-                        .padding(.top, 99.0)
+                        
                         
                     }
                     
                     
-                    
                 }
-                
-                
             }
         }
+        
     }
-            
-        }
-
+}
     
     struct SINGIN_Previews: PreviewProvider {
         static var previews: some View {
