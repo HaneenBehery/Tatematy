7c463badcaa3307441a63b0dcda7c0036b2ec6e2
Shifa <shifaabdulaziz@hotmail.com>
2022-11-05 21:49:46 +0300

    .

--- TEST/sittinges.swift
+++ TEST/sittinges.swift
@@ -41,7 +41,7 @@ struct sittinges: View {
                      //   }
                     })
                     
-                //    .navigationBarBackButtonHidden(true)
+                    .navigationBarBackButtonHidden(true)
                     
                    
                    
@@ -85,12 +85,12 @@ struct sittinges: View {
                                 .padding(.trailing , 10)
                                 .overlay {
                                     VStack{
-                                            Image("Image1")
+                                            Image("translation")
                                             .foregroundColor(Color.black)
-                                            .font(.system(size: 48))
+                                            .font(.system(size: 36))
                                             .padding([.top, .leading, .trailing], -5.0)
                                             .padding(3)
-                                        Text("Languuge")
+                                        Text("Language")
                                             .font(.system(size:21))
                                             .font(.callout)
                                             .foregroundColor(.black)
@@ -100,9 +100,10 @@ struct sittinges: View {
                                 }
                      //   }
                     
-                //    .navigationBarBackButtonHidden(true)
-                 //   .navigationBarBackButtonHidden(true)
+                    .navigationBarBackButtonHidden(true)
+                    .navigationBarBackButtonHidden(true)
                     
+                   
                    
                     NavigationLink(destination: SwiftUIView()
                         .navigationBarBackButtonHidden(true)) {
@@ -126,64 +127,35 @@ struct sittinges: View {
                                 .padding([.leading], -10) }
                         
                     }
-
-                        
-//                        RoundedRectangle(cornerRadius: 20, style: .continuous)
-//                            .stroke(Color(red: 0.904, green: 0.743, blue: 0.468), lineWidth: 7)
-//                            .frame(width: 167, height: 163)
-//                            .overlay {
-//
-//                                VStack{
-//                                    Image(systemName: "ellipsis.message")
-//                                        .foregroundColor(Color.black)
-//                                        .font(.system(size: 48))
-//                                        .padding([.top, .leading, .trailing], -5.0)
-//                                        .padding(3)
-//                                    Text("Common Questions")
-//                                        .font(.system(size:21))
-//                                        .font(.callout)
-//                                        .foregroundColor(.black)
-//                                        .multilineTextAlignment(.center)
-//                                    .padding(.bottom, -10.0)}
-//                                .padding([.leading], -10) }
-                        
-                    }
-                        
+                        .navigationBarBackButtonHidden(true)
                     
                         }
-
+                       
                         Spacer()
-                HStack {
-                    Button ("Log Out") {
-
-                    }
-                    .bold()
-                    .frame(width: 350, height: 44)
-                    .background(Color(red: 0.586, green: 0.75, blue: 0.683))
-                    .foregroundColor(.white)
-                    .cornerRadius(60)
-                    .font(.system(size: 23))
-                }
+                        HStack {
+                            Button ("Log Out") {
+                        
+                            }
+                            .bold()
+                            .frame(width: 350, height: 44)
+                            .background(Color(red: 0.586, green: 0.75, blue: 0.683))
+                            .foregroundColor(.white)
+                            .cornerRadius(60)
+                            .font(.system(size: 23))
                         }
-            
-            
-                        .navigationTitle("Setting")
-            
-                        .navigationBarTitleDisplayMode(.inline)
-            
-                        .navigationBarItems(
-                        leading: NavigationLink(destination:ContentView()) {
-                            Image(systemName: "chevron.backward.circle")
-                                .foregroundColor(.black)
-                        })
-                    
                 }
-               
-
-            
+                
+            .navigationTitle("Settings")
+                
+            .navigationBarTitleDisplayMode(.inline)
+                         .navigationBarItems(
+                         leading: NavigationLink(destination:ContentView().navigationBarHidden(true)) {
+                             Image(systemName: "chevron.backward.circle")
+                                 .foregroundColor(.black)
+                         })
             }
-        
-
+        }
+    }
 
 struct sittinges_Previews: PreviewProvider {
     static var previews: some View {
